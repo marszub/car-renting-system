f0f395e9fb6393b9cb5efec8213636d04821ee3c
Marcin Szubert <marszub@gmail.com>
2022-06-01 23:18:44 +0200

    Auth endpoints

---
+++ auth/auth/dto/RegisterData.cs
@@ -0,0 +1,18 @@
+﻿namespace auth.dto
+{
+    using System.ComponentModel.DataAnnotations;
+    public class RegisterData
+    {
+        [Required]
+        [StringLength(50, MinimumLength = 4)]
+        public string Login { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Mail { get; set; }
+
+        [Required]
+        [StringLength(200, MinimumLength = 8)]
+        public string Password { get; set; }
+    }
+}
