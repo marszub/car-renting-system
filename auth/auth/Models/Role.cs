a32625e2988f81fff59faaffef3358f8dca415ad
Marcin Szubert <marszub@gmail.com>
2022-06-02 23:42:10 +0200

    Ass data base models

---
+++ auth/auth/Models/Role.cs
@@ -0,0 +1,13 @@
+﻿namespace auth.Models
+{
+    using System.ComponentModel.DataAnnotations.Schema;
+
+    public class Role
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public int RoleID { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public ICollection<RolePossesion> RolePossesions { get; set; }
+    }
+}
---
+++ auth/auth/Models/RolePossesion.cs
@@ -0,0 +1,9 @@
+﻿namespace auth.Models
+{
+    public class RolePossesion
+    {
+        public int RolePossesionID { get; set; }
+        public User User { get; set; }
+        public Role Role { get; set; }
+    }
+}
