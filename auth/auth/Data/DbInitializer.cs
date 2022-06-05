bab354bad2f454c5de7d232a9f92402879cc1df4
Marcin Szubert <marszub@gmail.com>
2022-06-05 11:08:28 +0200

    Add db context

---
+++ auth/auth/Data/DbInitializer.cs
@@ -0,0 +1,14 @@
+﻿namespace auth.Data
+{
+    public static class DbInitializer
+    {
+        public static void Initialize(AuthContext context)
+        {
+            if (!context.Roles.Where(((Models.Role role) => role.Name == "User")).Any())
+            {
+                context.Roles.Add(new Models.Role(1, "User", "Regular user without additional privilages"));
+                context.SaveChanges();
+            }
+        }
+    }
+}
