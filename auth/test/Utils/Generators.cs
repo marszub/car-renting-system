8ece8ba48a611cf3d92c64ce9442002adb0013d5
Marcin Szubert <marszub@gmail.com>
2022-06-08 20:19:33 +0200

    HashGenerator

---
+++ auth/test/Utils/Generators.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test.Utils
+{
+    public class Generators
+    {
+        public static IEnumerable<string> NextStrings(Random rnd, string allowedChars, (int Min, int Max) length, int count)
+        {
+            ISet<string> usedRandomStrings = new HashSet<string>();
+            (int min, int max) = length;
+            char[] chars = new char[max];
+            int setLength = allowedChars.Length;
+
+            while (count-- > 0)
+            {
+                int stringLength = rnd.Next(min, max + 1);
+
+                for (int i = 0; i < stringLength; ++i)
+                {
+                    chars[i] = allowedChars[rnd.Next(setLength)];
+                }
+
+                string randomString = new string(chars, 0, stringLength);
+
+                if (usedRandomStrings.Add(randomString))
+                {
+                    yield return randomString;
+                }
+                else
+                {
+                    count++;
+                }
+            }
+        }
+    }
+}
