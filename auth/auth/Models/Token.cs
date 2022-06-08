dab81bf74139ac021677d51bbb69a41a1b0ac55d
Marcin Szubert <marszub@gmail.com>
2022-06-08 17:26:57 +0200

    Add Token model

---
+++ auth/auth/Models/Token.cs
@@ -0,0 +1,17 @@
+﻿namespace auth.Models
+{
+    using System.ComponentModel.DataAnnotations.Schema;
+
+    public class Token
+    {
+        public int TokenID { get; set; }
+        public User User { get; set; }
+        public string Value { get; set; }
+
+        public Token(User user, string value)
+        {
+            User = user;
+            Value = value;
+        }
+    }
+}
