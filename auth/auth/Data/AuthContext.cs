78984c90d3f9b4c9922b11d38c2025faded93318
Marcin Szubert <marszub@gmail.com>
2022-06-08 21:16:55 +0200

    CreateToken method

--- auth/auth/Data/AuthContext.cs
+++ auth/auth/Data/AuthContext.cs
@@ -4,6 +4,7 @@ using Microsoft.EntityFrameworkCore.Storage;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using auth.DataObject;
 using Microsoft.EntityFrameworkCore.ChangeTracking;
+using auth.Security;
 
 namespace auth.Data
 {
@@ -40,21 +41,29 @@ namespace auth.Data
                 throw new UserAlreadyExistsException();
             }
 
-            User user = new User(data.Login, data.Email, data.Password);
+            User user = new User(data.Login, data.Email, HashGenerator.GetHash(data.Password));
             user.Role = Roles.Single(role => role.Name == "User");
             Users.Add(user);
 
             SaveChanges();
         }
 
-/*        public string GetToken(LoginData data)
+        public string CreateToken(LoginData data)
         {
             User? user = GetVerifiedUser(data.Login, data.Password);
             if (user == null)
             {
                 throw new BadUserCredentialsException();
             }
-        }*/
+
+            string tokenValue = HashGenerator.GetHash(user.Login) + HashGenerator.GetHash(user.Email) + HashGenerator.GetHash(DateTime.Now.ToString());
+
+            Token token = new Token(tokenValue, user);
+            Tokens.Add(token);
+            SaveChanges();
+
+            return tokenValue;
+        }
 
         private bool UserExists(string Login, string Email)
         {
@@ -63,12 +72,15 @@ namespace auth.Data
                 || user.Email == Email).Any();
         }
 
-        /*private User? GetVerifiedUser(string UserId, string Password)
+        private User? GetVerifiedUser(string UserId, string Password)
         {
+            string hashedPassword = HashGenerator.GetHash(Password);
+
             return Users.Where((User user)
                 => (user.Login == UserId
-                || user.Email == UserId) 
-                && user.Password == Password).SingleOrDefault();
-        }*/
+                || user.Email == UserId)
+                && user.Password == hashedPassword)
+                .SingleOrDefault();
+        }
     }
 }
