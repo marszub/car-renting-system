f07f9a13122a7a02d46a1b0eb0064e7461f142ac
Marcin Szubert <marszub@gmail.com>
2022-06-17 00:28:14 +0200

    Don't wait for ice shutdown

--- auth/auth/Program.cs
+++ auth/auth/Program.cs
@@ -17,7 +17,6 @@ namespace auth
                     adapter.addDefaultServant(new AccountDefault(context), "account");
 
                     adapter.activate();
-                    communicator.waitForShutdown();
                 }
             }
             catch (Exception e)
