ab919e6c3c65f35d43557004c4467099da488321
JanuszJakubiec <54719269+JanuszJakubiec@users.noreply.github.com>
2022-07-25 17:52:47 +0200

    Adding checkstyle and tests

--- carDB/src/main/java/pl/agh/edu/cardatabase/config/WebSecurityConfig.java
+++ carDB/src/main/java/pl/agh/edu/cardatabase/config/WebSecurityConfig.java
@@ -2,16 +2,24 @@ package pl.agh.edu.cardatabase.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.HttpStatusEntryPoint;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+<<<<<<< Updated upstream
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
+=======
+import pl.agh.edu.cardatabase.car.auth.UserFilter;
+>>>>>>> Stashed changes
 
 import java.util.Arrays;
 
@@ -19,22 +27,45 @@ import java.util.Arrays;
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
+    private final UserFilter userFilter;
 
+    public WebSecurityConfig(final UserFilter userFilter) {
+        this.userFilter = userFilter;
+    }
+
+    @Override
+    protected void configure(final AuthenticationManagerBuilder auth) { }
+
+    @Override
+    protected void configure(final HttpSecurity http) throws Exception {
         http
+<<<<<<< Updated upstream
                 .csrf().disable().cors();
+=======
+                .cors().and()
+                .csrf().disable()
+                .addFilterBefore(userFilter, UsernamePasswordAuthenticationFilter.class)
+                .exceptionHandling()
+                .authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED))
+                .and().authorizeRequests().antMatchers("/api/*").authenticated()
+                .anyRequest().permitAll();
+>>>>>>> Stashed changes
     }
 
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
-        CorsConfiguration configuration = new CorsConfiguration();
+        final CorsConfiguration configuration = new CorsConfiguration();
         configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000"));
         configuration.setAllowCredentials(true);
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
+<<<<<<< Updated upstream
         configuration.setAllowedHeaders(Arrays.asList("authorization", "content-type", "x-auth-token", "userId", "token"));
+=======
+        configuration.setAllowedHeaders(Arrays.asList("authorization", "content-type", "userId",
+                "token"));
+>>>>>>> Stashed changes
         configuration.setExposedHeaders(Arrays.asList("x-auth-token"));
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
         return source;
     }
