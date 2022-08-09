0acd47e73978a85e0262d15707d18eee1a829a47
Marcin Szubert <marszub@gmail.com>
2022-08-09 22:26:08 +0200

    Add admin role

--- auth/Auth/Data/DbInitializer.cs
+++ auth/Auth/Data/DbInitializer.cs
@@ -4,11 +4,16 @@
     {
         public static void Initialize(AuthContext context)
         {
-            if (!context.Roles.Where((role => role.Name == "User")).Any())
+            if (!context.Roles.Where(role => role.Name == Role.User.ToString() || role.RoleID == 1).Any())
             {
                 context.Roles.Add(new Models.Role(1, Role.User.ToString(), "Regular user without additional privilages"));
-                context.SaveChanges();
             }
+            if (!context.Roles.Where(role => role.Name == Role.Admin.ToString() || role.RoleID == 2).Any())
+            {
+                context.Roles.Add(new Models.Role(2, Role.Admin.ToString(), "Admin with full access to application"));
+                context.TryAddUniqueUser("root", "", "password", Role.Admin);
+            }
+            context.SaveChanges();
         }
     }
 }
