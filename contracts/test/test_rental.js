d4c28c43094dab5592968ed03a5a89840ed7abc6
Antoni Gradowski <antoni.gradowski@gmail.com>
2022-08-18 21:40:20 +0200

    Finshed conract tests and fixed blockchain address

--- contracts/test/test_rental.js
+++ contracts/test/test_rental.js
@@ -27,7 +27,8 @@ contract("Rental", accounts => {
         assert.equal(result[1],2);
     });
 
-    it("should not another car with the same ID", async () => {
+    //this one is from web, passes at ANY error
+    it("should not add another car with the same ID", async () => {
       return this.rentalTest.addCar(1).then(() => {
         assert.ok(false, "It didn't fail");
       }, () => {
@@ -36,14 +37,31 @@ contract("Rental", accounts => {
     });
 
     it("should add active rental and get it", async () => {
-        await this.rentalTest.startRental(10,1,1);
+        const emit_event = await this.rentalTest.startRental(10,1,1);
+
+        const returned_id = emit_event.logs[0].args[0].toNumber();//the id of the rental
+
+        assert.equal(returned_id, 1)//should be 1, the first rental
 
         const result = await this.rentalTest.getActiveRental(1)
 
         assert.equal(result.rentTime, 10);
     });
 
+    //this one is from web, passes at ANY error
     it("should end rental and fail at getting it", async () => {
+      await this.rentalTest.endRental(1, 20)
+
+      return this.rentalTest.getActiveRental(1).then(() => {
+        assert.ok(false, "It didn't fail");
+      }, () => {
+        assert.ok(true, "Passed");
+      });
+    });
+      
+    //this one is from web, passes at ANY error
+    it("should fail at ending the same rental secind time", async () => {
+
       return this.rentalTest.endRental(1).then(() => {
         assert.ok(false, "It didn't fail");
       }, () => {
@@ -51,5 +69,21 @@ contract("Rental", accounts => {
       });
     });
 
-    
+    it("should return two cars (array of three, with two marked as free (so false)), after one has been rented", async () => {
+      await this.rentalTest.addCar(3);
+      await this.rentalTest.startRental(10,1,1);
+
+      const result = await this.rentalTest.getAllAvailableCars();
+      //cars
+      assert.equal(result[0][0],1);
+      assert.equal(result[0][1],2);
+      assert.equal(result[0][2],3);
+      //availability
+      assert.equal(result[1][0],true);
+      assert.equal(result[1][1],false);
+      assert.equal(result[1][2],false);
+      //assert.equal(result[1],2);  
+
   });
+
+});
