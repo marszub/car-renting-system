f5dcd7e28c8e259b0cece84d0c8ea79295feb73a
Marcin Szubert <54717847+marszub@users.noreply.github.com>
2022-09-18 17:11:32 +0200

    config clenup

--- user-frontend/src/config.js
+++ user-frontend/src/config.js
@@ -1,3 +1,3 @@
-export const AUTH_SERVICE = "http://localhost:3000/api"
+export const AUTH_SERVICE = "http://localhost:5010/api"
 export const CAR_DB_SERVICE = "http://localhost:5030/api"
 export const RENTAL_SERVICE = "http://localhost:5020/api"
